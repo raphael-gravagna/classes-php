b97cdbf3fea83e20eacfac4974aac92a5445ed5d
raphael gravagna <raphael.gravagna@laplateforme.io>
2021-12-17 08:38:59 +0100

    function get/login,email,firstname,lastname

--- user.php
+++ user.php
@@ -1,20 +1,37 @@
 <?php
 
 class User {
-    private $id;
     public $login;
     public $email;
     public $firstname;
     public $lastname;
 
-    public function __construct($id, $login, $email, $firstname, $lastname) {
-        $this->id = $id;
+    public function __construct($login, $email, $firstname, $lastname) {
         $this->login = $login;
         $this->email = $email;
         $this->firstname = $firstname;
-        $this->lastname = $lastname;
+        $this->lastname = $lastname;        
+    }
+
+    public function echologin() { 
+        echo nl2br("Login : ". $this->login."\n");        
+    }
+    public function echoemail() { 
+        echo nl2br("email : ". $this->email."\n");          
+    }
+    public function echofirstname() { 
+        echo nl2br("firstname : ". $this->firstname."\n"); 
+    }
+    public function echolastname() { 
+        echo nl2br("lastname : ". $this->lastname)."\n";         
     }
 
 }
+
+$user = new User("jojo", "jojo@aol.com", "Morera", "joan");
+$user->echologin();
+$user->echoemail();
+$user->echofirstname();
+$user->echolastname();
 
 ?>
